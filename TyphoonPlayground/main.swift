5179a9e9819d9c9a70ce7afaf50b881818bb2f71
Aleksey Garbarev <alex.garbarev@gmail.com>
2016-04-15 13:15:34 +0600

    Initial Commit

---
+++ TyphoonPlayground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TyphoonPlayground
+//
+//  Created by Aleksey Garbarev on 15/04/16.
+//  Copyright © 2016 Aleksey Garbarev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
