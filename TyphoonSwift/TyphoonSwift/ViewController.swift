6ed2ddb897fe6edb87cf39ac59a2c3cb133c98d8
Igor Vasilenko <ivasilenko@yotateam.com>
2016-07-04 15:56:32 +0300

    Reorganized framework folders and created example project

---
+++ TyphoonSwift/TyphoonSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TyphoonSwift
+//
+//  Created by Igor Vasilenko on 04/07/16.
+//  Copyright © 2016 Typhoon Framework. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
