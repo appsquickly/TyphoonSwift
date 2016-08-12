c3523ba2598714df263fe79730bab49b091c38d2
Igor Vasilenko <ivasilenko@yotateam.com>
2016-08-12 17:35:25 +0300

    Added macOS example project

---
+++ Example/TyphoonSwift-Example-MacOS/TyphoonSwift-Example-MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TyphoonSwift-Example-MacOS
+//
+//  Created by Igor Vasilenko on 12/08/16.
+//  Copyright © 2016 typhoonframework.org. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
