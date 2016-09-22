bca3a1b079adbd58cbdb45c5e8eb6ae35a242d09
Igor Vasilenko <ivasilenko@yotateam.com>
2016-09-22 12:12:35 +0300

    Added SwiftDocumentKey

---
+++ Sources/SwiftDocumentKey.swift
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  TYPHOON FRAMEWORK
+//  Copyright 2016, TyphoonSwift Framework Contributors
+//  All Rights Reserved.
+//
+//  NOTICE: The authors permit you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+import Foundation
+
+enum SwiftDocKey {
+    
+    // MARK: SourceKit Keys
+    
+    static let annotatedDeclaration = "key.annotated_decl"
+    static let attributes           = "key.attributes"
+    static let attribute            = "key.attribute"
+    static let bodyLength           = "key.bodylength"
+    static let bodyOffset           = "key.bodyoffset"
+    static let diagnosticStage      = "key.diagnostic_stage"
+    static let filePath             = "key.filepath"
+    static let fullXMLDocs          = "key.doc.full_as_xml"
+    static let inheritedTypes       = "key.inheritedtypes"
+    static let kind                 = "key.kind"
+    static let length               = "key.length"
+    static let name                 = "key.name"
+    static let nameLength           = "key.namelength"
+    static let nameOffset           = "key.nameoffset"
+    static let offset               = "key.offset"
+    static let substructure         = "key.substructure"
+    static let syntaxMap            = "key.syntaxmap"
+    static let typeName             = "key.typename"
+}
