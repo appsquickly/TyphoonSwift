5e882b73020147ca261d99fcc00a590ddd122038
Igor Vasilenko <ivasilenko@yotateam.com>
2016-09-22 12:23:33 +0300

    AssemblyDefinitionBuilder: replace hardcoded keys to enum

--- Sources/AssemblyDefinitionBuilder.swift
+++ Sources/AssemblyDefinitionBuilder.swift
@@ -24,13 +24,13 @@ class AssemblyDefinitionBuilder {
     
     func build() -> AssemblyDefinition?
     {
-        if let assemblyName = node["key.name"] as? String {
+        if let assemblyName = node[SwiftDocKey.name] as? String {
             
             let assembly = AssemblyDefinition(withName: assemblyName)
             
-            if let substructure = node["key.substructure"] as? [NSDictionary] {
+            if let substructure = node[SwiftDocKey.substructure] as? [NSDictionary] {
                 for item in substructure {
-                    if item["key.kind"] as! String == SourceLang.Declaration.instanceMethod {
+                    if item[SwiftDocKey.kind] as! String == SourceLang.Declaration.instanceMethod {
                         
                         let methodBuilder = MethodDefinitionBuilder(source: text, node: item)
                         
