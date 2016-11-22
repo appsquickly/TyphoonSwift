385745a72bec4fb95bce0f3f6f74c024edae907b
German Saprykin <saprykin.h@gmail.com>
2016-11-22 11:38:34 +0700

    Added workspace and separate project for dependencies.

--- Dependencies/Package.swift
+++ Dependencies/Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "TyphoonSwift",
+    name: "TyphoonSwiftDependencies",
     dependencies: [
             .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 6),
             .Package(url: "https://github.com/vasilenkoigor/Witness", majorVersion: 0, minor: 4)
